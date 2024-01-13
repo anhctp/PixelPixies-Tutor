950fd228b799c1bb882c488ded50b042aaf9119b
thuy <ntthuytq03@gmail.com>
2024-01-14 01:25:52 +0700

    add to create roadmap

--- server/controllers/roadmapController.py
+++ server/controllers/roadmapController.py
@@ -10,6 +10,8 @@ from ai.img2txt import detect_document_uri
 from ai.utils.extract_roadmap import extract_roadmap
 from models.marking import MarkingModel
 from ai.prompt.roadmap_prompt import roadmap_template
+from models.roadmap import RoadMapModel
+from models.step import StepModel
 
 class RoadMapController:
     def create_roadmap(topic: str, time: int, db: Session = Depends(getDatabase), current_user: UserModel = Depends(verifyToken)):
@@ -20,4 +22,24 @@ class RoadMapController:
         roadmap = get_chat(messages=chat_prompts)
         print(roadmap)
         result = extract_roadmap(roadmap)
+        new_roadmap = RoadMapModel(
+            user_id=current_user.id,
+            months=time
+        )
+        db.add(new_roadmap)
+        db.commit()
+        db.refresh(new_roadmap)
+        for roadmap in result:
+            new_step = StepModel(
+                roadmap_id = new_roadmap.id,
+                step_number = roadmap['step_number'],
+                resource_name = roadmap['resource name'],
+                resource_link = roadmap['resource link'],
+                task = roadmap['task'],
+                time = roadmap['time']
+            )
+            db.add(new_step)
+            db.commit()
+            db.refresh(new_step)
+            
         return result
